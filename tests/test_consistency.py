0e8568897041cd5d04ccc3213d19c2100a4f27c4
zhou jingyu <2601666034@qq.com>
2024-12-18 06:24:49 +0000

    add some files

--- tests/test_consistency.py
+++ tests/test_consistency.py
@@ -8,7 +8,7 @@ import clip
 
 @pytest.mark.parametrize('model_name', clip.available_models())
 def test_consistency(model_name):
-    device = "cpu"
+    device = "gpu"
     jit_model, transform = clip.load(model_name, device=device, jit=True)
     py_model, _ = clip.load(model_name, device=device, jit=False)
 
